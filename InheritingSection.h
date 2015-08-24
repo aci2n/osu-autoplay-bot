98dadf8827e6e733f71bf0985cd111ef1cfd8d8d
alvarocalace <alvicala_10@hotmail.com>
2015-08-24 15:11:42 -0300

    a

--- InheritingSection.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class InheritingSection
-{
-	int mFactor;
-public:
-	InheritingSection(int factor);
-	~InheritingSection();
-};
-
--- Program.cpp
+++
@@ -1,8 +0,0 @@
-#include "stdafx.h"
-#include "BeatmapParser.h"
-
-int main()
-{
-	BeatmapParser parser("C:/Program Files (x86)/osu!/Songs/3678 Tenacious D - Tribute/Tenacious D - Tribute (awp) [Easy].osu");
-	Beatmap beatmap = parser.parse_beatmap();
-}
